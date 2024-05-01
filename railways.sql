ff91fb32f145ba466f484f7bd2c7ad95dd07f4f8
Srikrishna Sripathi Nayak <117760225+Srikrishna-S-Nayak@users.noreply.github.com>
2024-05-01 18:29:01 +0530

    Add railway.sql file

---
+++ railways.sql
@@ -0,0 +1,12 @@
+USE railway_reservation;
+
+INSERT INTO trains (id, departure_time, arrival_time, available_seats, source, destination, train_name)
+VALUES
+    (16595, '18:50:00', '08:30:00', 220, 'KSR BENGALURU', 'KARWAR', 'PANCHAGANGA EXP'),
+    (16596, '18:00:00', '07:15:00', 220, 'KARWAR', 'KSR BENGALURU', 'PANCHAGANGA EXP'),
+    (16565, '23:55:00', '16:15:00', 200, 'KSR BENGALURU', 'MANGALURU CNTL', 'YPR MAQ EXP'),
+    (16566, '20:05:00', '15:00:00', 200, 'MANGALURU CNTL', 'KSR BENGALURU', 'MAQ YPR EXP'),
+    (11302, '20:40:00', '20:15:00', 325, 'KSR BENGALURU', 'C SHIVAJI MAH TERM', 'UDYAN EXPRESS'),
+    (11301, '08:15:00', '06:15:00', 325, 'C SHIVAJI MAH TERM', 'KSR BENGALURU', 'UDYAN EXPRESS');
+    
+
